2ce24f5b4b3a6f6c3007095b674a317d94fce9ec
kane abdoul <kaneaziz@gmail.com>
2023-11-14 14:54:17 +0100

    list tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
+  end
 end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,32 @@
+class TasksController < ApplicationController
+
+def index
+  @tasks = Task.all
+end
+
+
+  def show
+    # Implement logic to view details of a task
+  end
+
+  def new
+    # Implement logic to render a form for a new task
+  end
+
+  def create
+    # Implement logic to create a new task
+  end
+
+  def edit
+    # Implement logic to render a form to edit a task
+  end
+
+  def update
+    # Implement logic to update a task
+  end
+
+  def destroy
+    # Implement logic to remove a task
+  end
+
+end
